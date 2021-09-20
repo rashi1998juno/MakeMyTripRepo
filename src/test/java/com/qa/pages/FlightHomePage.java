4636c323edb8f12e9ae7db1d0396b711bdfffb4a
HP <HP@DESKTOP-9LF9B7F>
2021-09-20 22:44:35 +0530

    login page committing 2nd commit

--- src/test/java/com/qa/pages/FlightHomePage.java
+++ src/test/java/com/qa/pages/FlightHomePage.java
@@ -5,14 +5,109 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class MakeMyTripHomePage {
+public class FlightHomePage {
 	
 	WebDriver driver;
 	 
-	 @FindBy(xpath="//p[@xpath='1']")
-    WebElement Loginbtn;
+	
+	@FindBy(xpath = "//*[@id='SW']/div[1]/div[1]/ul/li[3]")
+    WebElement Loginfield;
 	 
-	 public MakeMyTripHomePage(WebDriver driver) {
+	 public WebElement get_Login_btn() throws Exception 
+		{
+		 Thread.sleep(3000);
+		 return  Loginfield;
+	}
+	 	 
+	 
+//------------------------------------------------------------------------------------------------//	 
+	 
+	 @FindBy(xpath = "//*[@id='username']")
+	    WebElement emailfield;
+		 
+		 public WebElement get_email_btn() throws Exception 
+			{
+			 Thread.sleep(3000);
+			 return  emailfield;
+		}
+		 
+//------------------------------------------------------------------------------------------------//	 
+ 
+	 @FindBy(xpath="//*[@id='SW']/div[1]/div[2]/div[2]/section/form/div[2]/button")
+	    WebElement continubtn;
+		 
+		 public WebElement continue_btn() 
+			{
+		 
+			 return continubtn ;
+			}
+//------------------------------------------------------------------------------------------------//		 
+		 		 
+		 
+		 
+		 @FindBy(xpath="//*[@id='password']")
+		    WebElement password;
+			 
+			 public WebElement pswd_field() 
+				{
+			 
+				 return password ;
+				}
+
+//------------------------------------------------------------------------------------------------//			 
+			 
+			 
+			 
+//			 @FindBy(xpath="//*[@id='SW']/div[1]/div[2]/div[2]/section/form/div[2]/button")
+			 
+			// @FindBy(xpath="//*[@id='SW']/div[1]/div[2]/div[2]/section/form/div[2]/button/span")
+			 @FindBy(xpath="//body/div[@id='root']/div[1]/div[1]/div[1]/div[2]/div[2]/section[1]/form[1]/div[2]")
+			    WebElement loginbtn;
+				 
+				 public WebElement login_btn() 
+					{
+				 
+					 return loginbtn ;
+					}
+				 
+//	-------------------------------------------------------------------------------------------------//
+				 
+				 
+//				 
+//				 @FindBy(xpath="//*[@id='SW']//div[1]/div[2]/div[2]/section/div[1]/div/span")
+//				    WebElement closePopup;
+//					 
+//					 public WebElement popup_field() 
+//						{
+//					 
+//						 return closePopup ;
+//						}
+		 
+//------------------------------------------------------------------------------------------------	//	 
+//		  
+//	 @FindBy(xpath="//input[@id='fromCity']")
+//	    WebElement FromCity;
+//		 
+//		 public WebElement from_city()
+//			{
+//			 
+//				return  FromCity ;
+//			}
+//		 
+		 
+		 
+//		 
+//		 @FindBy(xpath="//p[@xpath='1']")
+//		    WebElement Loginbtn;
+//			 
+//			 public WebElement get_Login_btn()
+//				{
+//					return  Loginbtn;
+//				}
+//			 
+	 
+	 
+	 public FlightHomePage(WebDriver driver) {
 			this.driver=driver;
 	    	PageFactory.initElements(driver, this);
 	    	  
