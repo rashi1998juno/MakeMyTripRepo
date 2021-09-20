6add1386d925434865d54f128bd408702f3a265f
HP <HP@DESKTOP-9LF9B7F>
2021-09-20 22:36:18 +0530

    Login code added commiting 1st time

--- src/test/java/com/qa/testscript/TestBase.java
+++ src/test/java/com/qa/testscript/TestBase.java
@@ -1,37 +1,65 @@
 package com.qa.testscript;
 
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-import com.qa.pages.MakeMyTripHomePage;
+import com.qa.pages.FlightHomePage;
 
 public class TestBase {
-	
-	
-	WebDriver driver;
-	MakeMyTripHomePage Mor;
 
+	public static WebDriver driver;
+	public FlightHomePage For = new FlightHomePage(driver);
+	
 	// 1.Open a browser
+
 	@BeforeClass
 	@Parameters({ "Browser", "Url" })
-	public void setUp(String Browser, String Url) {
+	public void setUp(String Browser, String Url) throws Exception {
 		// 1.Open a browser
 		if (Browser.equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver",
 					"C:\\Users\\HP\\Videos\\selenium_jar\\chromedriver_win32 (2)\\chromedriver.exe");
 			driver = new ChromeDriver();
 		}
-		
-		Mor = new MakeMyTripHomePage(driver);
+
+		For = new FlightHomePage(driver);
 		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 
 		// 2.launching the url
 		driver.get(Url);
 
-	}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		For = new FlightHomePage(driver);
+		For.get_Login_btn().click();
+
+		Thread.sleep(3000);
+		For.get_Login_btn().click();
+		Thread.sleep(2000);
+
+	
+		//Alert alert = driver.switchTo().alert();
+		//alert.accept();
+		For.get_email_btn().sendKeys("srashi801@gmail.com");
+		Thread.sleep(3000);
+		
+		For.continue_btn().click();
+		//Thread.sleep(3000);
+		For.pswd_field().sendKeys("Raksha@2002");
+		Thread.sleep(3000);
+		For.login_btn().click();
+	
+}
+		
 
 	@AfterClass
 	public void tearDown() {
@@ -40,19 +68,4 @@ public class TestBase {
 
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
